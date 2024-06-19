d8798d9e799523a6b1ecde284f9f5168514d3f8b
ShodytheWolf <martin.antunez5@gmail.com>
2024-06-19 15:54:49 -0300

    added Dependency Injections for Task and Project
    ad

--- app/repositories.php
+++ app/repositories.php
@@ -1,18 +1,17 @@
 <?php
 
 use DI\ContainerBuilder;
+use App\Infrastructure\Persistence\ProjectRepositoryImpl;
 use App\Infrastructure\Persistence\TaskRepositoryImpl;
 use App\Infrastructure\Persistence\UserRepositoryImpl;
 use App\Domain\Repositories\UserRepository;
 use App\Domain\Repositories\TaskRepository;
+use App\Domain\Repositories\ProjectRepository;
 
 return function (ContainerBuilder $containerBuilder){
     $containerBuilder->addDefinitions([
-        UserRepository::class => \DI\autowire(UserRepositoryImpl::class)
+        UserRepository::class => \DI\autowire(UserRepositoryImpl::class),
+        TaskRepository::class => \DI\autowire(TaskRepositoryImpl::class),
+        ProjectRepository::class => \DI\autowire(ProjectRepositoryImpl::class)
     ]);
-
-    $containerBuilder->addDefinitions([
-        TaskRepository::class => \DI\autowire(TaskRepositoryImpl::class)
-    ]);
-
 };
