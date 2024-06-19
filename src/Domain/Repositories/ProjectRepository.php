cfa070b3a3d5d9dec6fe40e8c6ba2ddedb17fcfb
Elinzar <88413253+Elinzar@users.noreply.github.com>
2024-06-19 15:53:46 -0300

    push by id

--- src/Domain/Repositories/ProjectRepository.php
+++ src/Domain/Repositories/ProjectRepository.php
@@ -4,13 +4,16 @@ namespace App\Domain\Repositories;
 
 //use App\Domain\Entities\Project;
 
+use App\Domain\Entities\Project;
+use App\Infrastructure\Persistence\ProjectRepositoryImpl;
+
 interface ProjectRepository
 {
 
     public  function createProject();
     public  function editProject();
     public  function deleteProject();
-    public function findById();
+    public function findById(int $id):?Project;
     public function findAll();
 
 }
